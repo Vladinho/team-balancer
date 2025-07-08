c7a202d12fbe70aafc10f2db9628b733787feb1d
VladRepkin <repkin.vladislav@leaflink.com>
2025-07-08 14:08:09 +0300

    init 8

--- src/components/TeamsDisplay.tsx
+++ src/components/TeamsDisplay.tsx
@@ -14,13 +14,14 @@ export const TeamsDisplay: React.FC<Props> = ({ teams }) => (
                 <Col key={idx} xs={12} md={6} className="mb-3">
                     <div className="bg-secondary p-3 rounded text-light">
                         <h5 className="text-center">Команда {idx + 1}</h5>
-                        <ul className="list-unstyled mb-0">
+                        <ul className="list-unstyled mb-3">
                             {team.map(p => (
                                 <li key={p.id}>
                                     {p.name} {p.nickname && `(${p.nickname})`}
                                 </li>
                             ))}
                         </ul>
+                        <h5>Суммарный рейтинг: {team.reduce((acc, player) => acc + player.rating, 0)}</h5>
                     </div>
                 </Col>
             ))}
